fc8baf8be772673f6c220335098d3b62dcf564cc
Keshav <kshavgupta47@gmail.com>
2019-09-29 15:25:52 +0530

    aaa

--- playground.py
+++ playground.py
@@ -80,15 +80,15 @@ def main():
     vocab = pickle.load(open('/home/jupyter/filestore/keshav/vsepp/data/fashion/fashion_vocab.pkl', 'rb'))
     opt.vocab_size = len(vocab)
     print(opt)
-    checkpoint = torch.load("model_best.pth.tar")
+    checkpoint = torch.load("/home/jupyter/filestore/keshav/vsepp/weights/exp1_finetune_2/model_best.pth.tar")
     model = VSE(opt)
     model.load_state_dict(checkpoint['model'])
 
     train_loader, val_loader = data.get_loaders(
         opt.data_name, vocab, opt.crop_size, opt.batch_size, opt.workers, opt)
 
-    image_array = numpy.zeros((7998,1024))
-    text_array = numpy.zeros((7998,1024))
+    image_array = numpy.zeros((13707,1024))
+    text_array = numpy.zeros((13707,1024))
     for i, (images, captions, lengths, ids) in enumerate(train_loader):
         img_emb, cap_emb = model.forward_emb(images, captions, lengths,
                                              volatile=True)
